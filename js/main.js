d4b6a98a6aa1b50c3bac451e33f6045a612e7723
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-02-01 09:12:06 +0300

    правки после ревью

--- js/main.js
+++ js/main.js
@@ -1,43 +1,24 @@
-const getRandomIntInclusive = function (min, max) {
-  min = Math.abs(Math.ceil(min));
-  max = Math.abs(Math.floor(max));
+function getRandomNumber(min, max) {
+  min = Math.abs(+min);
+  max = Math.abs(+max);
   if (min > max) {
-    let swap = min;
+    const swap = min;
     min = max;
     max = swap;
   }
-  // console.log(min);
-  // console.log(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.random() * (max - min + 1) + min;
 }
 
-getRandomIntInclusive(32.1324354, 0);
-// let result = getRandomIntInclusive(32.1324354, 0);
-// console.log(result);
-
-
-// source
-//https://www.jacklmoore.com/notes/rounding-in-javascript/
-
-const round = function(min, max, decimals) {
-
-  min = Math.abs(min);
-  max = Math.abs(max);
-
-  if (min > max) {
-    let swap = min;
-    min = max;
-    max = swap;
-  }
-  // console.log(min);
-  // console.log(max);
-
-  let value = Math.floor(Math.random() * (max - min + 1)) + min;
-  // console.log(value);
-
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+function getRandomInt(min, max) {
+  const randomInt = getRandomNumber (min, max);
+  return Math.floor(randomInt);
 }
 
-round(-3.54657, 1.26567653, 2)
-// let result2 = round(-3.54657, 1.26567653, 2);
-// console.log(result2);
+function getRandomFloat(min, max, decimal = 2) {
+  const randomFloat = getRandomNumber(min,  max);
+  return Number(randomFloat.toFixed(decimal));
+}
+
+getRandomNumber(32.1324354, 0);
+getRandomInt(32.1324354, 0);
+getRandomFloat(32.1324354, 0, 4);
