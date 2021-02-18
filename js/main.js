bc0923e447dbd46ba8071256876fa956119ed9da
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-02-18 12:12:51 +0300

    feat: add min offer price by type & sync of check-in check-out times

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { offers } from  './mock.js';
-import { createCard } from'./card.js';
+import { createCard } from './card.js';
+import { updateOfferPrice, updateCheckTime } from './form.js';
 
 createCard();
 
