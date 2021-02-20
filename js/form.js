f988e17fffa3f359f6884634ce0d8e2608b0c1c3
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-02-20 18:41:32 +0300

    refactor:trying to connect leaflet.js

--- js/form.js
+++ js/form.js
@@ -32,10 +32,32 @@ const updateCheckTime = (evt) => {
 };
 
 //Обработчики событий
-const updateFormInput = () => {
+const setFormInputHandlers = () => {
   typeSelect.addEventListener('input', updateOfferPrice);
   timeInSelect.addEventListener('input', updateCheckTime);
   timeOutSelect.addEventListener('input', updateCheckTime);
 }
 
-export { updateFormInput }
+//Делает форма объявления и все поля неактивными
+const getAdFormInactive = () => {
+  const adForm = document.querySelector('.ad-form');
+  const adFormHeader = adForm.querySelector('.ad-form-header');
+  const adFormElements = adForm.querySelectorAll('.ad-form__element');
+
+  adForm.classList.add('ad-form--disabled');
+  adFormHeader.disabled = true;
+  adFormElements.forEach(formElement => formElement.disabled = true);
+}
+
+// Делает фильтрацию объявлений и все поля неактивными
+const getMapFiltersInactive = () => {
+  const mapFilters = document.querySelector('.map__filters');
+  const mapFiltersIds = mapFilters.querySelectorAll('.map__filter');
+  const mapFeatures = mapFilters.querySelector('.map__features');
+
+  mapFilters.classList.add('map__filters--disabled');
+  mapFiltersIds.forEach(mapFilterId => mapFilterId.disabled = true);
+  mapFeatures.disabled = true;
+}
+
+export { setFormInputHandlers }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import { createOffers } from  './mock.js';
 import { createCard } from './card.js';
-import { updateFormInput } from './form.js';
+import { setFormInputHandlers } from './form.js';
+import  { getAdFormInactive, getMapFiltersInactive} from './map.js';
+
 
 createOffers();
-createCard();
-updateFormInput();
+// createCard();
+setFormInputHandlers();
 
 
 
