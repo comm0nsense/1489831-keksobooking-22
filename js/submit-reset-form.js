a0472668e551aefba133c17ff7873e24462d3578
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-03-04 12:33:50 +0300

    refactor: move setDefaults() inside submit-reset-form

--- js/submit-reset-form.js
+++ js/submit-reset-form.js
@@ -6,22 +6,6 @@ import {  } from './map.js';
 // Константы
 const POST_DATA_URL = 'https://22.javascript.pages.academy/keksobooking';
 
-//Обработчик события submit
-const submitAdForm = (callback) => {
-  adForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
-    const formData = new FormData(evt.target);
-    postData(POST_DATA_URL, formData)
-      .then(() => showModal(newSuccessModal))
-      .catch(() => showModal(newErrorModal));
-
-    callback();
-
-  });
-}
-
-
 //Сброс формы и фильтров
 const setDefaults = () => {
   adForm.reset();
@@ -29,6 +13,22 @@ const setDefaults = () => {
   // mainPin.setLatLng([35.6804, 139.759]);
 }
 
-submitAdForm(setDefaults);
+//Обработчик события submit
+const submitAdForm = () => {
+  adForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.target);
+    postData(POST_DATA_URL, formData)
+      .then(() => {
+        showModal(newSuccessModal);
+        setDefaults();
+      })
+      .catch(() => showModal(newErrorModal));
+  });
+}
+
+
+submitAdForm();
 
 export { setDefaults, submitAdForm };
